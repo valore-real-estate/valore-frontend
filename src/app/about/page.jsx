f9e65d19bf311774025aa4978ee98bb6d4f7357c
lizzaaabb <lbogveradze12@gmail.com>
2026-06-06 00:30:36 +0400

    about

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import About from '../../components/About'
+
+export const metadata = {
+  title: 'About | Valore Real Estate',
+  description: 'Learn more about the Valore real estate team.',
+}
+
+export default function AboutPage() {
+  return (
+    <About />
+  )
+}
